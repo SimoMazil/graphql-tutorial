16dfc8dac5c8eb330cf56c3b07e11036c9c7e68e
Simo Mazil <mohamed.mazil@gmail.com>
2019-05-09 15:19:33 +0000

    connected to mongodb

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const graphqlHTTP = require("express-graphql")
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express()
+
+const url = 'mongodb://localhost:27017/test'
+mongoose.connect(url, { useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+  console.log("Connected to DB");
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
