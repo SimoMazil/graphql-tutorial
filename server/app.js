b44603f4311a211eb8b8725593d7071ceda82bda
Simo Mazil <mohamed.mazil@gmail.com>
2019-05-11 16:35:57 +0000

    add cors package to allow cross-origin requests

--- server/app.js
+++ server/app.js
@@ -2,13 +2,17 @@ const express = require("express")
 const graphqlHTTP = require("express-graphql")
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
+
+// Allow cross-origin requests
+app.use(cors())
 
 const url = 'mongodb://localhost:27017/graphqlDB'
 mongoose.connect(url, { useNewUrlParser: true })
 mongoose.connection.once('open', () => {
-  console.log("Connected to DB");
+  console.log("Connected to DB")
 })
 
 app.use('/graphql', graphqlHTTP({
