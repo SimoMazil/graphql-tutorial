6500ab9d200dd48edb5ef39d45fd10fa4ef2a0ec
Simo Mazil <mohamed.mazil@gmail.com>
2019-05-11 18:38:22 +0000

    add a state and some events

--- client/src/componenets/AddBook.js
+++ client/src/componenets/AddBook.js
@@ -4,6 +4,12 @@ import { graphql } from 'react-apollo';
 import { getAuthorsQuery } from '../queries/queries';
 
 class AddBook extends Component {
+  state = {
+    name: null,
+    genre: null,
+    authorId: null
+  }
+  
   displayAuthors() {
     const {loading, authors} = this.props.data;
 
@@ -20,23 +26,29 @@ class AddBook extends Component {
     }
   }
 
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
     return (
-      <form id='add-book'>
+      <form id='add-book' onSubmit={this.onSubmit}>
 
         <div className='field'>
           <label>Book Name</label>
-          <input type='text'/>
+          <input type='text' onChange={(e) => this.setState({name: e.target.value})}/>
         </div>
 
         <div className='field'>
           <label>Book Genre</label>
-          <input type='text'/>
+          <input type='text' onChange={(e) => this.setState({name: e.target.value})}/>
         </div>
 
         <div className='field'>
           <label>Author</label>
-          <select>
+          <select onChange={(e) => this.setState({name: e.target.value})}>
+            <option>Select Author</option>
             {this.displayAuthors()}
           </select>
         </div>
