94788d114a7394507d7057392746762780d0a9ea
Simo Mazil <mohamed.mazil@gmail.com>
2019-05-11 18:26:08 +0000

    external queries

--- client/src/componenets/AddBook.js
+++ client/src/componenets/AddBook.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
-import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
 
-const getAuthorsQuery = gql`
-  {
-    authors {
-      name
-      id
-    }
-  }
-`
+import { getAuthorsQuery } from '../queries/queries';
 
 class AddBook extends Component {
-
   displayAuthors() {
     const {loading, authors} = this.props.data;
 
